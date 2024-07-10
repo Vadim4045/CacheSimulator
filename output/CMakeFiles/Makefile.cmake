38dc86cd780355873f151afc9f1fa3f56485d283
Vadim Natin <focusdigit@gmail.com>
2024-07-10 10:27:00 +0300

    CMake & gitignor

--- output/CMakeFiles/Makefile.cmake
+++ output/CMakeFiles/Makefile.cmake
@@ -40,5 +40,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/CacheSimulator.dir/DependInfo.cmake"
+  "CMakeFiles/CacheSimulator.bin.dir/DependInfo.cmake"
   )
